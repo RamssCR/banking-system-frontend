6af63cdf6851ab5c10db069c6763d6b2b095b3f7
RamssCR <ramsesgpm@gmail.com>
2025-10-18 16:23:57 -0500

    feat: added global toaster

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import './index.css'
 import { App } from './App.tsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Toaster } from '@shared/ui/primitives/Toaster.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Toaster />
   </StrictMode>,
 )
