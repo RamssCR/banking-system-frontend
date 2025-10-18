14363001ad3dc3559daddd6c08fe1b881f5f5348
RamssCR <ramsesgpm@gmail.com>
2025-10-18 16:25:57 -0500

    feat: used components as samples

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { Button } from '@shared/ui/primitives/Button'
+import { Spinner } from '@shared/ui/primitives/Spinner'
 import { Switch } from '@shared/ui/primitives/Switch'
+import { toast } from 'sonner'
 
 export const App = () => {
   return (
     <div className="p-5">
       <Switch />
+      <Spinner />
+      <Button
+        onClick={() => {
+          toast.success('Toast triggered!', {
+            description: 'This is a sample toast notification.',
+          })
+        }}
+      >
+        Trigger Toast
+      </Button>
     </div>
   )
 }
