f0c9aa61417383bcd6595f7f8c322d23f0927494
RamssCR <ramsesgpm@gmail.com>
2025-10-21 21:07:30 -0500

    feat: used app as playground for new views

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
-import { Button } from '@shared/ui/primitives/Button'
-import { Input } from '@shared/ui/primitives/Input'
-import { Radio, RadioGroup } from '@shared/ui/primitives/Radio'
-import { Spinner } from '@shared/ui/primitives/Spinner'
-import { Switch } from '@shared/ui/primitives/Switch'
-import { toast } from 'sonner'
+import { Loading } from '@shared/views/Loading'
+import { BrowserRouter } from 'react-router-dom'
 
 export const App = () => {
   return (
-    <div className="p-5">
-      <Switch />
-      <Spinner />
-      <RadioGroup defaultValue="React">
-        <Radio value="React" variant="primary" />
-        <Radio value="Vue" variant="primary" />
-        <Radio value="Angular" variant="primary" />
-        <Radio value="Svelte" variant="primary" />
-      </RadioGroup>
-      <Input type="file" />
-      <Button
-        onClick={() => {
-          toast.success('Toast triggered!', {
-            description: 'This is a sample toast notification.',
-          })
-        }}
-      >
-        Trigger Toast
-      </Button>
-    </div>
+    <BrowserRouter>
+      <Loading />
+    </BrowserRouter>
   )
 }
