ad88ef11ee424c6356a08847348adde11b61ce32
RamssCR <ramsesgpm@gmail.com>
2025-10-25 15:04:23 -0500

    feat: added a constant for request timeout

--- src/shared/utils/constants.ts
+++ src/shared/utils/constants.ts
@@ -7,7 +7,7 @@ const CURRENT_YEAR = new Date().getFullYear()
  * A constant representing the duration for fade-in and
  * fade-out animations in milliseconds.
  */
-const FADE_IN_OUT_DURATION_MS = 3000
+const FADE_IN_OUT_DURATION_MS = 5000
 
 /**
  * A constant representing the duration for CSS fade-in and
@@ -15,4 +15,14 @@ const FADE_IN_OUT_DURATION_MS = 3000
  */
 const CSS_FADE_IN_OUT_DURATION_MS = 500
 
-export { CURRENT_YEAR, FADE_IN_OUT_DURATION_MS, CSS_FADE_IN_OUT_DURATION_MS }
+/**
+ * A constant representing the request timeout duration in milliseconds.
+ */
+const REQUEST_TIMEOUT_MS = 10000
+
+export {
+  CURRENT_YEAR,
+  FADE_IN_OUT_DURATION_MS,
+  CSS_FADE_IN_OUT_DURATION_MS,
+  REQUEST_TIMEOUT_MS,
+}
