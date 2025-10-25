38f17085f767ca2424102aa5d9320b75371ac92e
RamssCR <ramsesgpm@gmail.com>
2025-10-25 15:08:11 -0500

    feat: created requests for authentication

---
+++ src/features/auth/services/authorization.ts
@@ -0,0 +1,43 @@
+import {
+  type Response,
+  response as responseSchema,
+} from '@features/auth/schemas/backend'
+import type { SignIn, SignUp } from '@features/auth/schemas/auth'
+import { api } from '@shared/helpers/api'
+
+/**
+ * Creates a new user account and signs the user in.
+ * @param data - User credentials for signing in.
+ * @returns User data along with a success message.
+ */
+export const signUp = async (data: SignUp): Promise<Response> => {
+  const response = await api.post<unknown>('/auth/signup', data)
+  return responseSchema.parse(response.data)
+}
+
+/**
+ * Signs user into his account.
+ * @param data - User credentials for signing in.
+ * @returns User data along with a success message.
+ */
+export const signIn = async (data: SignIn): Promise<Response> => {
+  const response = await api.post<unknown>('/auth/signin', data)
+  return responseSchema.parse(response.data)
+}
+
+/**
+ * Fetches the profile of the currently authenticated user.
+ * @returns User data along with a success message.
+ */
+export const profile = async (): Promise<Response['user']> => {
+  const response = await api.get<unknown>('/auth/profile')
+  return responseSchema.parse(response.data).user
+}
+
+/**
+ * Signs the user out of his account.
+ * @returns A promise that resolves when the user is signed out.
+ */
+export const signOut = async (): Promise<void> => {
+  await api.post('/auth/signout')
+}
